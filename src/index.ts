eb000564f56f471c9c089640a421ea94ea6145a5
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-05-04 18:44:47 +0100

    Uncomment client initialization and event registration code; configure dotenv and login

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-// import { Client, GatewayIntentBits, Partials, Options } from "discord.js";
-// import dotenv from "dotenv";
-// import { registerEvents } from "./events";
+import { Client, GatewayIntentBits, Partials, Options } from "discord.js";
+import dotenv from "dotenv";
+import { registerEvents } from "./events";
 
-// dotenv.config();
+dotenv.config();
 
-// const client = new Client({
-//   intents: [
-//     GatewayIntentBits.Guilds,
-//     GatewayIntentBits.GuildVoiceStates,
-//     GatewayIntentBits.GuildMembers,
-//   ],
-//   partials: [Partials.GuildMember, Partials.Channel],
-//   makeCache: Options.cacheWithLimits({
-//     VoiceStateManager: 50,
-//   }),
-// });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMembers,
+  ],
+  partials: [Partials.Channel],
+});
 
-// client.login(process.env.DISCORD_TOKEN);
-console.log("hello");
+client.login(process.env.DISCORD_TOKEN);
